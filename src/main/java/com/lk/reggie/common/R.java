901ea3428483a86c75e3d0443948c68945231795
luo <1261219286@qq.com>
2024-06-24 15:41:02 +0800

    spring cache缓存数据

--- src/main/java/com/lk/reggie/common/R.java
+++ src/main/java/com/lk/reggie/common/R.java
@@ -1,16 +1,19 @@
 package com.lk.reggie.common;
 
 import lombok.Data;
+
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * 通用返回结果，服务端响应数据最终都会封装为此对象
+ *
  * @param <T>
  */
 
 @Data
-public class R<T> {
+public class R<T> implements Serializable {
 
     private Integer code; //编码：1成功，0和其它数字为失败
 
